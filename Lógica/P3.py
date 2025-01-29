124f45ec6a78c5d02dc136ce046e1ddd83fcc1da
manulaz <manuel7super@gmail.com>
2025-01-29 01:19:43 -0300

    a

---
+++ Lógica/P3.py
@@ -0,0 +1,45 @@
+#While Loops
+#While - Enquanto
+
+# Inicializa a variável 'numero' com o valor 1
+numero = 1     
+
+# Continua o loop enquanto 'numero' for menor que 5
+while numero < 5:
+    
+    # Imprime o valor atual de 'numero'
+    print(numero)
+    
+    # Incrementa 'numero' em 1 para a próxima iteração
+    #numero = numero + 1
+    numero += 1
+
+
+#Contar de 4 a 1
+contador = 4
+
+# Enquanto a variável contador for maior ou igual a 1, o loop continuará
+while contador >= 1:
+    
+    # Imprime o valor atual da variável contador
+    print(contador)  
+    
+    # Decrementa o valor da variável contador em 1
+    #contador = contador - 1
+    contador -= 1
+    
+
+#Contar de 10 a 1 de 2 em 2
+
+# Inicializa a variável contador com o valor 10
+contador = 10
+
+# Enquanto a variável contador for maior ou igual a 1, o loop continuará
+while contador >= 1:
+    
+    # Imprime o valor atual da variável contador
+    print(contador)  
+    
+    # Decrementa o valor da variável contador em 2
+    contador -= 2 
+
